013ef2237f9061ca556af0af0f2fed12a38c7ebf
Josh Ike <mail@chukwuanu.com>
2020-12-21 02:49:46 +0100

    update test suite

--- src/index.php
+++ src/index.php
@@ -1,7 +1,27 @@
 <?php
 
+function fibonacci($input)
+{
+  $arr = array();
 
+  for ($i=0; $i < $input; $i++)
+  {
+    if ($i === 0 || $i === 1){
+      $arr[$i] = $i;
+    } else {
+      $arr[$i] = $arr[$i - 1] + $arr[$i - 2];
+    }
+  }
 
-function solution() {
-  return "HELLO";
+  return $arr;
 }
+
+function solution($input)
+{
+  // Solution
+  return fibonacci($input);
+}
+
+
+$input = (int)$argv[1];
+echo solution($input);
